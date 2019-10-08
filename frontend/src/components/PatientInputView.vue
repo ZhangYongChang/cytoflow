c2361b3f55254edc533bc5534d2b7f52253eeb80
yczhang <yongchangzhang@163.com>
2019-10-08 13:28:12 +0800

    inputview

--- frontend/src/components/PatientInputView.vue
+++ frontend/src/components/PatientInputView.vue
@@ -1,197 +1,93 @@
 <template>
   <div>
-    <form
-      action="/api/create_patient"
-      method="POST"
-      enctype="application/x-www-form-urlencoded"
-    >
-      <vue-form-generator
-        :schema="schema"
-        :model="model"
-        :options="formOptions"
-      ></vue-form-generator>
-      <input
-        @click="onCommit"
-        type="submit"
-        value="提交"
-      >
-    </form>
+    <el-form ref="form" :model="form" label-width="80px" size="medium" hide-required-asterisk=false>
+      <el-form-item label="送检人信息">
+        <el-row :gutter="20">
+          <el-col :span="11">
+            <el-form-item label="姓名">
+              <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="性别">
+              <el-select v-model="form.sex" placeholder="请选择性别">
+                <el-option label="男" value="Men"></el-option>
+                <el-option label="女" value="Male"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="年龄">
+              <el-input v-model="form.age"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form-item>
+      <el-form-item label="标本信息">
+        <el-row :gutter="20">
+          <el-col :span="11">
+            <el-form-item label="标本编号">
+              <el-input v-model="form.specimenno"></el-input>
+            </el-form-item>
+            <el-form-item label="医院">
+              <el-input v-model="form.hospital"></el-input>
+            </el-form-item>
+            <el-form-item label="科室">
+              <el-input v-model="form.department"></el-input>
+            </el-form-item>
+            <el-form-item label="床号">
+              <el-input v-model="form.bedno"></el-input>
+            </el-form-item>
+            <el-form-item label="医生">
+              <el-input v-model="form.doctor"></el-input>
+            </el-form-item>
+            <el-form-item label="标本类型">
+              <el-select v-model="form.specimentype" placeholder="请选择标本类型">
+                <el-option label="骨髓" value="GUSHUI"></el-option>
+                <el-option label="尿液" value="NIAOYE"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="送检时间">
+              <el-date-picker v-model="form.collecttime" type="datetime" placeholder="选择送检时间">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item label="接收时间">
+              <el-date-picker v-model="form.recvtime" type="datetime" placeholder="选择接收时间">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onCommit">添加</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import VueFormGenerator from 'vue-form-generator'
-import 'vue-form-generator/dist/vfg.css'
-
 export default {
   name: 'PatientInputView',
   components: {
-    'vue-form-generator': VueFormGenerator.component
   },
   data () {
     return {
-      model: {
-        name: '张三',
-        sex: '男',
-        age: 30,
-        specimenno: 'AAAA01',
-        hospital: '湖北省人民医院',
-        department: '00A',
-        bedno: '010',
-        doctor: '李四',
-        specimentype: '骨髓',
-        caseno: '01010A',
-        collecttime: '2014-07-01',
-        recvtime: '2014-07-02'
+      form: {
+        name: '',
+        sex: '',
+        age: 0,
+        specimenno: '',
+        hospital: '',
+        department: '',
+        bedno: '',
+        doctor: '',
+        specimentype: '',
+        caseno: '',
+        collecttime: '',
+        recvtime: ''
       },
-      result: {},
-      schema: {
-        groups: [
-          {
-            legend: '基本信息',
-            fields: [
-              {
-                type: 'input',
-                inputType: 'text',
-                label: 'Name',
-                model: 'name',
-                readonly: false,
-                featured: true,
-                required: true,
-                disabled: false,
-                placeholder: '姓名',
-                validator: VueFormGenerator.validators.string
-              },
-              {
-                type: 'select',
-                label: 'Sex',
-                model: 'sex',
-                multi: false,
-                required: true,
-                multiSelect: false,
-                values: ['男', '女', ' ']
-              },
-              {
-                type: 'input',
-                inputType: 'number',
-                label: 'Age',
-                model: 'age',
-                min: 0,
-                validator: VueFormGenerator.validators.number
-              }
-            ]
-          },
-          {
-            legend: '标本信息',
-            fields: [
-              {
-                type: 'input',
-                inputType: 'text',
-                label: '标本编号',
-                model: 'specimenno',
-                readonly: false,
-                featured: true,
-                required: true,
-                disabled: false,
-                placeholder: '标本编号',
-                validator: VueFormGenerator.validators.string
-              },
-              {
-                type: 'input',
-                inputType: 'text',
-                label: '送检医院',
-                model: 'hospital',
-                readonly: false,
-                featured: true,
-                required: true,
-                disabled: false,
-                placeholder: '送检医院',
-                validator: VueFormGenerator.validators.string
-              },
-              {
-                type: 'input',
-                inputType: 'text',
-                label: '科室',
-                model: 'department',
-                readonly: false,
-                featured: true,
-                required: true,
-                disabled: false,
-                placeholder: '科室',
-                validator: VueFormGenerator.validators.string
-              },
-              {
-                type: 'input',
-                inputType: 'text',
-                label: '床号',
-                model: 'bedno',
-                readonly: false,
-                featured: true,
-                required: true,
-                disabled: false,
-                placeholder: '床号',
-                validator: VueFormGenerator.validators.string
-              },
-              {
-                type: 'input',
-                inputType: 'text',
-                label: '送检医生',
-                model: 'doctor',
-                readonly: false,
-                featured: true,
-                required: true,
-                disabled: false,
-                placeholder: '送检医生',
-                validator: VueFormGenerator.validators.string
-              },
-              {
-                type: 'select',
-                label: '标本类型',
-                model: 'specimentype',
-                multi: false,
-                required: true,
-                multiSelect: false,
-                values: ['骨髓', '血液', '尿液']
-              },
-              {
-                type: 'input',
-                inputType: 'text',
-                label: '病例号',
-                model: 'caseno',
-                readonly: false,
-                featured: true,
-                required: true,
-                disabled: false,
-                placeholder: '病例号',
-                validator: VueFormGenerator.validators.string
-              },
-              {
-                type: 'dateTimePicker',
-                label: '采集时间',
-                model: 'collecttime',
-                required: true,
-                placeholder: '采集时间'
-              },
-              {
-                type: 'dateTimePicker',
-                label: '接收时间',
-                model: 'recvtime',
-                required: true,
-                placeholder: '接收时间'
-              }
-            ]
-          }
-        ]
-      },
-      formOptions: {
-        validateAfterLoad: true,
-        validateAfterChanged: true
-      }
+      result: {}
     }
   },
   methods: {
     onCommit () {
-      this.$axios.post('/api/create_patient', this.model)
+      this.$axios.post('/api/create_patient', this.form)
         .then(response => (this.result = response['data']))
         .catch(function (error) { console.log(error) })
     }
