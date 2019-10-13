58c0272895bdf7f679cd3e38f7491a6d638c4e5a
hubo <hubo@egova.com.cn>
2019-10-13 19:56:05 +0800

    实现多边形绘制交互

--- frontend/src/components/Experiment.vue
+++ frontend/src/components/Experiment.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import echarts from 'echarts'
+
 export default {
   name: 'Experiment',
   props: {
@@ -211,6 +213,59 @@ export default {
       }
     }
 
+    function registerGateDraw (zr) {
+      var drawing
+      var gate = []
+      var points
+      var traceEl
+      zr.on('mousedown', function (e) {
+        if (!drawing) {
+          drawing = true
+          gate.push([])
+          points = gate[gate.length - 1]
+        }
+      })
+      zr.on('mousemove', function (e) {
+        if (drawing && points.length > 0) {
+          traceEl.attr('shape', {
+            x1: points[points.length - 1][0],
+            y1: points[points.length - 1][1],
+            x2: e.offsetX,
+            y2: e.offsetY
+          })
+        }
+      })
+
+      zr.on('mouseup', function (e) {
+        points.push([e.offsetX, e.offsetY])
+        traceEl = new echarts.graphic.Line({
+          shape: {
+            x1: 0, y1: 0, x2: 0, y2: 0
+          },
+          style: {
+            stroke: 'green',
+            lineWidth: 1
+          }
+        })
+        zr.add(traceEl)
+      })
+
+      zr.on('dblclick', function (e) {
+        if (points.length < 4 || !drawing) {
+          return
+        }
+        points.length -= 1
+        drawing = false
+        traceEl.attr('shape', {
+          x1: points[points.length - 1][0],
+          y1: points[points.length - 1][1],
+          x2: points[0][0],
+          y2: points[0][1]
+        })
+        console.log(gate)
+      })
+    }
+
     let option = createOption(this.type, this.xaxis, this.yaxis, this.points)
     let myChart = this.$echarts.init(document.getElementById(this.viewid), 'shine')
     if (option && typeof option === 'object') {
@@ -219,6 +274,7 @@ export default {
         console.log(params)
       })
     }
+    registerGateDraw(myChart.getZr())
   }
 }
 </script>
